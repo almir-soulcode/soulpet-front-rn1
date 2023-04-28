776ee299e0b22a6affeb974ebb44b4f4d96a6af5
julybarros <julianajulyy22@gmail.com>
2023-04-28 13:48:43 -0300

    pagina detalhe cliente

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home/Home";
 import { NovoCliente } from "./pages/NovoCliente/NovoCliente";
 import { Clientes } from "./pages/Clientes/Clientes";
 import { EditaCliente } from "./pages/EditaCliente/EditaCliente";
+import { DetalheClientes } from "./pages/DetalheClientes/DetalheClientes";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/clientes" element={<Clientes />} />
           <Route path="/clientes/novo" element={<NovoCliente />} />
           <Route path="/clientes/editar/:id" element={<EditaCliente />} />
+          <Route path="/clientes/detalhes/:id" element={<DetalheClientes/>} />
         </Route>
       </Routes>
     </BrowserRouter>
